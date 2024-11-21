f834eccce97fe1b5cc78ce5866430e84887c3989
Krishna94 <krishnavenigoud745@gmail.com>
2024-11-21 11:20:19 +0530

    Added login page

---
+++ SwiftUIApp1/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  SwiftUIApp1
+//
+//  Created by RajKrishna on 21/11/24.
+//
+
+import SwiftUI
+
+struct LoginVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginVC()
+}
